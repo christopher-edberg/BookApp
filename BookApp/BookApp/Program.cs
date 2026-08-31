c9a25932988f51fd19d69c3404cda775c3d96491
christopher-edberg <31663888+christopher-edberg@users.noreply.github.com>
2026-08-31 22:01:32 +0200

    Add database migration on application startup

--- BookApp/BookApp/Program.cs
+++ BookApp/BookApp/Program.cs
@@ -44,7 +44,11 @@ builder.Services.AddCors(options =>
 
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseCors("AngularPolicy");
 //app.UseHttpsRedirection();
